ca965f5adcb2c1eed042e42f19efb559ebccc3a6
David Cyubahiro <156189366+DavidDau@users.noreply.github.com>
2024-11-20 13:36:09 +0200

    Added Aboutus v1

--- frontend/src/components/Aboutus.jsx
+++ frontend/src/components/Aboutus.jsx
@@ -5,23 +5,39 @@ function Aboutus() {
   return (
     <div className="about-us-container">
       <h1>About Us</h1>
+      <h2>Brief background</h2>
       <p>
-        We are a passionate team dedicated to creating awesome web applications
-        using the latest technologies. Our goal is to deliver high-quality
-        solutions that help businesses grow and succeed in the digital world.
+        Animal Solution Feedmill (ASF) produces high-quality, animal-specific
+        feed designed to enhance livestock production among small, medium, and
+        large-scale farmers in Nigeria at affordable prices. The company
+        provides technical expertise and consultation services to help farmers
+        with optimal feed formulation (ingredient mixtures) and nutrient
+        composition, supporting maximum livestock yield and growth. Through
+        effective nutrition, ASF's services help to reduce high mortality rates,
+        increase productivity, and improve returns on investment in livestock
+        production.
       </p>
       <h2>Our Mission</h2>
       <p>
-        Our mission is to build innovative and user-friendly websites that not
-        only meet but exceed the expectations of our clients. We believe in
-        continuous improvement and staying ahead of the curve in web development
-        trends.
+        To produce high-quality, affordable animal feed that supports the growth
+        and health of livestock, enabling farmers to achieve maximum
+        productivity and profitability. We are committed to innovation,
+        sustainability, and providing expert guidance to help farmers thrive in
+        a competitive agricultural environment.
+      </p>
+      <h2>Our Vision</h2>
+      <p>
+        To be the leading provider of innovative, high-quality animal feed
+        solutions that empower Nigerian farmers to achieve sustainable,
+        profitable, and responsible livestock production, supported by expert
+        technical advisory services.
       </p>
       <h2>Meet the Team</h2>
       <ul>
-        <li>John Doe - CEO</li>
-        <li>Jane Smith - Lead Developer</li>
-        <li>Bob Brown - UX/UI Designer</li>
+        <li>Emmanuel OLADEJO- CEO</li>
+        <li>Caline UWINGABIRE - Project maanager</li>
+        <li>Peter NMESOMA - Lead Developer</li>
+        <li>Raissa IRUTINGABO - UX/UI Designer</li>
       </ul>
     </div>
   );
